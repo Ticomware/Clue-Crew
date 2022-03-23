78ad1231005c8b3d16e6637f94815a4981361e6b
Rodrigo Rodriguez <rod19016@byui.edu>
2022-03-23 17:35:16 -0600

    final product.

--- questionMaker.py
+++ questionMaker.py
@@ -335,7 +335,8 @@ class AdvancedEditBoard(tk.Frame):
         text_file = open(text_file,'w')
         text_file.write(self.st.get(1.0, tk.END))
         text_file.close()
-        
+
+
 class EditBoard(tk.Frame):
 
     def __init__(self, parent, controller):
